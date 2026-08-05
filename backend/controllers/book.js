c973392f8fa68459c991cf36c5a95fc7e48de2e6
Arnaud Piette <arno.piette@gmail.com>
2026-08-05 12:14:39 +0200

    Création des contrôleurs publics des livres

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,1 +1,36 @@
+const Book = require('../models/Book');
 
+exports.getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    return res.status(200).json(books);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getOneBook = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({ message: 'Livre introuvable' });
+    }
+
+    return res.status(200).json(book);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getBestRatedBooks = async (req, res) => {
+  try {
+    const books = await Book.find()
+      .sort({ averageRating: -1 })
+      .limit(3);
+
+    return res.status(200).json(books);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
