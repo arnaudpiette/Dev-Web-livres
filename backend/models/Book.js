4ed21e40be07ad76f1016c7e07ea2a6304025252
Arnaud Piette <arno.piette@gmail.com>
2026-08-05 12:09:31 +0200

    Création des modèles User et Book

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,1 +1,62 @@
+const mongoose = require('mongoose');
 
+const ratingSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    grade: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+    },
+  },
+  { _id: false },
+);
+
+const bookSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    ratings: {
+      type: [ratingSchema],
+      default: [],
+    },
+    averageRating: {
+      type: Number,
+      min: 0,
+      max: 5,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model('Book', bookSchema);
