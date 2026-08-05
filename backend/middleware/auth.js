6032b580b4c8349cdfede2b65cb2da83108d18d7
Arnaud Piette <arno.piette@gmail.com>
2026-08-05 12:12:48 +0200

    Implémentation des routes CRUD des livres

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,21 @@
+const jwt = require('jsonwebtoken');
 
+module.exports = (req, res, next) => {
+  try {
+    const [type, token] = req.headers.authorization?.split(' ') || [];
+
+    if (type !== 'Bearer' || !token) {
+      return res.status(401).json({ message: 'Authentification requise' });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.auth = {
+      userId: decodedToken.userId,
+    };
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Token invalide ou expiré' });
+  }
+};
